42ed4b56f897b905ecd7692dc386da9c65b7cb5c
Laguette William <jawlaguette@gmail.com>
2022-07-20 18:48:59 +0400

    add errors
    the addition of errors in the code

--- addTwoInt.py
+++ addTwoInt.py
@@ -9,7 +9,21 @@ def add(a,b):
     x = a + b
     print(x)
 
+try:
+    a = int( sys.argv[1] )
+    b = int( sys.argv[2] )
+
+except IndexError:
+    print ("Error. 2 numerical valors")
+    a = int(input("Entrez la premiere valeur : "))
+    b = int(input("Entrez la dexieme valeur : "))
+    add(a,b)
+    raise (SystemExit)
+    
+
 a = int( sys.argv[1] )
 b = int( sys.argv[2] )
 
 add(a,b)
+
+
